502fe3306fa12fc4d069d27f46444a8f5ce02cb5
Marco <sonni46marcosenpai@gmail.com>
2021-09-20 14:03:50 +0200

    update

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1>prova</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <img src="" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
