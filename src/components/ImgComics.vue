49bfc73e31263599778bf2a3c70f76bc504688d9
Marco <sonni46marcosenpai@gmail.com>
2021-09-21 18:39:02 +0200

    "finito"

---
+++ src/components/ImgComics.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="img-comics">
+    <img :src="prod.thumb" alt="">
+    <p>{{prod.series}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["prod"],
+}
+</script>
+
+<style>
+ .img-comics  img {
+                width: 90%;
+                height: 25vh;
+                object-fit: cover;
+                object-position: top;
+        } 
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,16 @@
 <template>
   <main class="pos-rel z-index ">
     <div class="background-black">
-      <div class="container font-s-50 pad-30">
-        <span> -- Content goes hear -- </span>
+      <div class="jumbotron">
+          <img src="@/assets/img/jumbotron.jpg" alt="">
+      </div>
+      <div class="container pad-30">
+        <div class="cdComicImg">
+          <imgComic v-for="(img,index) in dcImgs" :key="index" :prod="img"/>
+        </div>
+        <div class="loadMore">
+          <span>LOAD MORE</span>
+        </div>
       </div>
     </div>
     <div class="background-blu-light pad-50">
@@ -10,7 +18,7 @@
         <ul class="comics">
           <li v-for="(img,index) in imgs" :key="index">
             <img :src="require(`@/assets/img/${img.url}`)" alt="">
-            <div>
+            <div class="more">
               <span>{{img.title}}</span>
             </div>
           </li>
@@ -21,12 +29,19 @@
 </template>
 
 <script>
-import * as Img from '@/assets/data/Imgs.js'
+import imgComic from "@/components/ImgComics.vue";
+import dcComics from "@/assets/data/dcComics.js";
+import * as Img from '@/assets/data/Imgs.js';
 export default {
   name: 'Main',
+  components:{
+    imgComic
+  },
+  dcComics,
   data() {
     return {
-      imgs : Img.mainImg
+      imgs : Img.mainImg,
+      dcImgs : dcComics,
     }
   }
 }
@@ -36,5 +51,4 @@ export default {
 <style scoped lang="scss">
 @import "../style/Utilitis";
 @import "../style/Main";
-
 </style>
